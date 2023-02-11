4fb02d92687e58225922da76573595374b5c617c
royce-mathew <royce1mathew@gmail.com>
2023-02-11 02:39:25 -0500

    Fixed routes

--- backend/blueprints/notify.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-notify = Blueprint(__name__, "notify")
-
-notify.route("/")
-def index():
-    return "Connected endpoints: ";
-
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, json, Blueprint
+# from werkzeug.exceptions import HTTPException
+import socket
 
+
+# Import blueprints
+from blueprints.browser_endpoint import browser
+
+# Initialize App
 app = Flask(__name__)
-# app.register_blueprint(views, url_prefix="/notify")
+hostname = socket.getfqdn() # Get the hostname
 
-@app.route("/")
+# Register Blueprints
+app.register_blueprint(browser, url_prefix="/api/browser")
+
+@app.route("/api/")
 def index():
-    print("")
-    return 0;
+    print("i love wome")
+    return "Ok", 200;
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(host=socket.gethostbyname_ex(hostname)[2][0], debug=True, port=8080)
