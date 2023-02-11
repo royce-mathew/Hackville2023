5df647b1fa2f214bea1673ba834c6d0917042695
Michaelexe <michaelispahani@gmail.com>
2023-02-11 08:42:39 -0500

    added a lot more

---
+++ frontend/components/BrightnessSlider.js
@@ -0,0 +1,54 @@
+import {View, StyleSheet, Image} from 'react-native';
+import {Slider} from 'native-base';
+import React from 'react';
+import axios from 'axios';
+import brightnessPNG from '../assets/brightness.png';
+
+const BrightnessSlider = () => {
+  const onChangeHandler = brightness => {
+    axios
+      .get(`http://${ip}/api/sliders/brightness?p=${Math.floor(brightness)}`)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  return (
+    <View style={styles.brightnessSlider}>
+      <Image
+        source={brightnessPNG}
+        style={{height: 40, width: 40, marginRight: 10}}
+      />
+      <View style={{flex: 1}}>
+        <Slider
+          defaultValue={70}
+          minValue={0}
+          maxValue={100}
+          step={10}
+          onChangeEnd={brightness => {
+            onChangeHandler(brightness);
+          }}>
+          <Slider.Track>
+            <Slider.FilledTrack />
+          </Slider.Track>
+          <Slider.Thumb />
+        </Slider>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  brightnessSlider: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    paddingHorizontal: 10,
+  },
+});
+
+export default BrightnessSlider;
