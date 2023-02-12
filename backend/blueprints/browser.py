7997d8680e0724dd84885985cc03db637540b854
AviShangari <shangariaaveg@gmail.com>
2023-02-11 19:49:45 -0500

    Updated and fixed bugs on all tutorial/blueprint files

--- backend/blueprints/browser.py
+++ backend/blueprints/browser.py
@@ -1,9 +1,16 @@
 from flask import Blueprint
+from utils import browser
 
 browser = Blueprint("browser", __name__)
 
-@browser.route("/")
+@browser.route("/open/")
 def index():
+    browser.open_browser()
     # Call browser method for opening / closing browser
-    return "Connected endpoints: ", 200;
+    return "Connected endpoints: ", 200
+
+@browser.route("/close/")
+def index2():
+    browser.close_browser()
+    return "Connected endpoints: ", 200
 
